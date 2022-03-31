fcb30fc311189ac498d92569341a4c23b4c0d0b0
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-03-31 15:30:56 +0900

    add update

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//LOGIN
+//JWTを使った LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
@@ -47,10 +47,11 @@ router.post("/login", async (req, res) => {
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
-    // res.status(200).json({ ...others, accessToken });
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 module.exports = router;
+
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,4 +1,3 @@
-const req = require("express/lib/request");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
