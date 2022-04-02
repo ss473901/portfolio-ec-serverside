ada8254421a711bdbee4188c4adb58dce38a07e6
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-04-02 18:27:55 +0900

    Add update create

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productsRoute = require("./routes/product");
+
 app.use(express.json());
 
 dotenv.config();
@@ -20,6 +22,7 @@ mongoose
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productsRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("サーバーが起動しました。");
